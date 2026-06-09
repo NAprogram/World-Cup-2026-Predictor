0485dee0a4d59857585bdcf87ccb2c56c256ef01
NAprogram <mrahmadnaim@gmail.com>
2026-06-09 15:58:58 +0400

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -50,7 +50,7 @@ app.post('/api/simulate', (req, res) => {
 app.use(express.static(path.join(__dirname, '../frontend/dist')));
 
 // Handle React routing, return all requests to React app
-app.get('*', (req, res) => {
+app.use((req, res) => {
     res.sendFile(path.join(__dirname, '../frontend/dist/index.html'));
 });
 
